b1383993db3f1967bff4ac535cf64c0c448fa003
Pascual Cano <pascual@allhuman.com>
2022-12-08 05:53:47 +0100

    Games without extra points are correctly managed

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export const getBowlingScore = (game: string): number => {
 
 const getFramePoints = (frame: string): number => {
 	const rolls = frame.split("-");
+	if (rolls[0] === "X") return 10;
 	if (rolls[1] === "/") return 10;
 	const rollsPoints = rolls.map(roll => parseInt(roll));
 	return rollsPoints.reduce((total, roll) => total + roll, 0);
--- tests/bowling.spec.ts
+++ tests/bowling.spec.ts
@@ -19,14 +19,26 @@ describe("Bowling game tests", () => {
 		expect(score).toBe(20);
 	});
 
-	test(`Game with a spare is correctly managed`, () => {
+	test(`Game with just a spare scores 10`, () => {
 		const game = "1-/ 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0";
 		const score = getBowlingScore(game);
 		expect(score).toBe(10);
 	});
 
-	test(`Game with two spares without extra points is correctly managed`, () => {
+	test(`Game with two spares without extra points scores 20`, () => {
 		const game = "1-/ 0-0 0-/ 0-0 0-0 0-0 0-0 0-0 0-0 0-0";
+		const score = getBowlingScore(game);
+		expect(score).toBe(20);
+	});
+
+	test(`Game with a single strike and the rest misses scores 10`, () => {
+		const game = "X 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0";
+		const score = getBowlingScore(game);
+		expect(score).toBe(10);
+	});
+
+	test(`Game with two strikes but no extra poinst scores 20`, () => {
+		const game = "X 0-0 X 0-0 0-0 0-0 0-0 0-0 0-0 0-0";
 		const score = getBowlingScore(game);
 		expect(score).toBe(20);
 	});
