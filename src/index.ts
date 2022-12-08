ffa0eba95ec891e2cce02834d45236b0b257a68b
Pascual Cano <pascual@allhuman.com>
2022-12-08 05:39:18 +0100

    Manage games without spares or strikes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export const getBowlingScore = (game: string): number => {
-	return 0;
+	const frames = game.split(" ");
+
+	const framesPoints = frames.map(frame => {
+		const rolls = frame.split("-");
+		const rollsPoints = rolls.map(roll => parseInt(roll));
+		return rollsPoints.reduce((total, roll) => total + roll, 0);
+	});
+
+	return framesPoints.reduce((total, framePoints) => total + framePoints, 0);
 };
--- tests/bowling.spec.ts
+++ tests/bowling.spec.ts
@@ -6,4 +6,10 @@ describe("Bowling game tests", () => {
 		const score = getBowlingScore(game);
 		expect(score).toBe(0);
 	});
+
+	test(`Game with just one pin down scores 1 point`, () => {
+		const game = "1-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0";
+		const score = getBowlingScore(game);
+		expect(score).toBe(1);
+	});
 });
