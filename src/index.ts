84d11249da04591f600fb3de7a02d4cb07635cd6
Pascual Cano <pascual@allhuman.com>
2022-12-09 12:14:49 +0100

    Calculate points just from knocked pins

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export const getBowlingScore = (game: string): number => {
-	return 0;
+	const frames = game.split(" ");
+	return frames
+		.map(frame => {
+			const rolls = frame.split("-");
+			const points = rolls.map(roll => parseInt(roll));
+			return points[0] + points[1];
+		})
+		.reduce((score, points) => score + points, 0);
 };
--- tests/bowling.spec.ts
+++ tests/bowling.spec.ts
@@ -5,4 +5,9 @@ describe("Bowling game tests", () => {
 		const score = getBowlingScore("0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0 0-0");
 		expect(score).toBe(0);
 	});
+
+	test(`When you knock 1 pin per frame the score is 10`, () => {
+		const score = getBowlingScore("1-0 1-0 1-0 1-0 1-0 1-0 1-0 1-0 1-0 1-0");
+		expect(score).toBe(10);
+	});
 });
